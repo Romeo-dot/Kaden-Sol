8a496a8e2576a3c49733a61c32dcbe885a26956d
Romeo-dot <106849194+Romeo-dot@users.noreply.github.com>
2025-02-22 12:03:54 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Kaden',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Sol Bot",
+  botOwner: 'Sol', // Your name
+  ownerNumber: '76959435', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
